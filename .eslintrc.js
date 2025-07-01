3cf3eaf934bb3d72db719889bfe8d75ffc393278
Arrnv <119531646+Arrnv@users.noreply.github.com>
2025-07-01 22:47:09 +0530

    apple 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
+  extends: ['next/core-web-vitals', 'eslint:recommended'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'react-hooks/rules-of-hooks': 'warn',
+    '@next/next/no-img-element': 'off',
   },
 };
