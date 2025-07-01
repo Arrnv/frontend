c1babbd8e0ea845ac1319097f9706738e1f3ff94
Arrnv <119531646+Arrnv@users.noreply.github.com>
2025-07-01 23:23:30 +0530

    no rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "react-hooks/rules-of-hooks": "warn",
+      'react/no-unescaped-entities': 'off',
       "@next/next/no-img-element": "off"
     }
   }
