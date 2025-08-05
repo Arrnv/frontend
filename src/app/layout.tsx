980a2079fa44d60934d382d1fe82cdf30aa0db4d
Arrnv <119531646+Arrnv@users.noreply.github.com>
2025-08-05 18:39:45 +0530

    fevicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdOrganization) }}
